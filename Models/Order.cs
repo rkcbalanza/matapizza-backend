8aaf05cf1c4652b6cd186ee7783fa0c58d54cd7d
rkcbalanza <richard.balanza@pointwest.com.ph>
2025-05-10 17:11:07 +0800

    *created Models *created DbContext

---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace MataPizza.Backend.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public TimeSpan OrderTime { get; set; }
+
+        public ICollection<OrderDetail> OrderDetails { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,46 @@
+using MataPizza.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Allow Frontend to access the API
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<MataPizzaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Configure the app to use Scalar in development mode
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = "MataPizza API Reference";
+        options.Theme = ScalarTheme.Moon;
+    });
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
